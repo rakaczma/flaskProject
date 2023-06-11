49d9a1556439bb4807184ac62b9e5e209578fca9
rakaczma <rakaczma@student.wszib.edu.pl>
2023-06-11 14:31:18 +0200

    o

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, abort, url_for
 from datetime import datetime
 import random
-from model.country import db, find_by_name
+from model.country import db, find_by_name, find_by_index, find_continent_by_cc, continent_map
 
 
 app = Flask(__name__)
+
 
 
 @app.route('/')
@@ -43,9 +44,46 @@ def random_country():
 def country_by_name(name: str):
     try:
         found_country = find_by_name(name)
-    except ValueError:
-        abort(404)
+    except ValueError as ex:
+        abort(404, ex)
     return render_template('country.html', country=found_country)
+
+@app.route('/countries/index/<int:index>')
+def country_by_index(index: int):
+    try:
+        found_country = find_by_index(index)
+        found_country['continent'] = find_continent_by_cc(found_country['cc'])
+    except IndexError:
+        abort(404, f'Country by index {index} can not be found!')
+    return render_template('country_index.html', country=found_country, index=index, continent_map=continent_map)
+
+
+colors = ['red', 'blue', 'green', 'yellow']
+
+class Square:
+
+    def __init__(self):
+        self.color = colors[random.randint(0, len(colors) - 1)]
+        self.counter = 0
+
+squares = []
+
+def create_squares():
+    global squares
+    squares = []
+    for i in range(9):
+        squares.append(Square())
+
+@app.route('/square')
+def create_square():
+    create_squares()
+    return render_template('square.html', squares=squares)
+
+@app.route('/square/<int:index>')
+def increment_counter_square(index: int):
+    found_square = squares[index]
+    found_square.counter = found_square.counter + 1
+    return render_template('square.html', squares=squares)
 
 print(app.url_map)
 
--- model/country.py
+++ model/country.py
@@ -1,4 +1,5 @@
 import json
+import pycountry_convert
 
 def load_db():
     with open('country_info.json', encoding='utf-8') as mock_database:
@@ -11,5 +12,18 @@ def find_by_name(name: str):
             return country_data
     raise ValueError(f"Country {name} not found")
 
+def find_by_index(index: int):
+    return db[index]
 
+def find_continent_by_cc(country_code):
+    try:
+        continent_code = pycountry_convert.country_alpha2_to_continent_code(country_code)
+        continent_name = pycountry_convert.convert_continent_code_to_continent_name(continent_code)
+        counter = continent_map.get(continent_name, 0)
+        continent_map[continent_name] = counter + 1
+        return continent_name
+    except KeyError:
+        return None
+
+continent_map = {}
 db = load_db()
